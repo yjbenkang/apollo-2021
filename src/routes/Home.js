fea487379a896d99fd7c50132bba2ccfd758e718
강용진 <kyongjin1234@gmail.com>
2021-09-01 19:19:39 +0900

    #1.1 GET MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,24 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => <h1>Home</h1>;
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
+
+export default () => {
+    const { loading, data } = useQuery(GET_MOVIES);
+    if (loading) {
+      return "loading...";
+    }
+    if (data && data.movies) {
+      return data.movies.map(m => <h1>{m.title}</h1>);
+    }
+  };
