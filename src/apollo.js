f26e0061616369000bd008946612cc40ab7f1ecc
강용진 <kyongjin1234@gmail.com>
2021-09-02 18:12:10 +0900

    #2.1 Local State part Two

--- src/apollo.js
+++ src/apollo.js
@@ -6,6 +6,23 @@ const client = new ApolloClient({
     resolvers: {
         Movie: {
           isLiked: () => false
+        },
+        Mutation: {
+            likeMovie: (_, { id, isLiked}, { cache }) => {
+                const myMovie = {
+                  __typename: 'Movie',
+                  id: `${id}`,
+                  isLiked: `${isLiked}`,
+                };
+                cache.modify({
+                  id: cache.identify(myMovie),
+                  fields: {
+                    isLiked() {
+                      return !isLiked;
+                    },
+                  },
+                });
+            }
         }
     }
 });
