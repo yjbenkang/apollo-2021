fcb7d99462e64240524ded8f7d0acee217488a92
강용진 <kyongjin1234@gmail.com>
2021-09-02 15:09:30 +0900

    #1.3 Apollo Cache and Styles

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -9,9 +10,45 @@ const GET_MOVIE = gql`
       id
       title
       medium_cover_image
+      language
+      rating
       description_intro
     }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
 `;
 
 export default () => {
@@ -19,10 +56,24 @@ export default () => {
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id: +id }
   });
-  if(loading) {
-    return "loading..."
+  if(loading){
+    return (
+    <Container>
+    </Container>
+  );
   }
-  if(data && data.movie) {
-    return <div>{data.movie.title}</div>
+  if(data && data.movie){
+      console.log(data.movie);
+      return (
+        <Container>
+          <Column>
+            <Title>{data.movie.title}</Title>
+            <Subtitle>{data.movie.language}/⭐️{data.movie.rating}/</Subtitle>
+            <Description>{data.movie.description_intro}</Description>
+          </Column>
+          <Poster></Poster>
+        </Container>
+      );
   }
+  
 };
