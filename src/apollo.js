8f423679665cda1cc7f78e8c16c9f231f94ababd
강용진 <kyongjin1234@gmail.com>
2021-09-02 17:36:34 +0900

    #2.0 Local State part One

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,13 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient,InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-uri: "http://localhost:4000/",
-cache: new InMemoryCache(),
+    uri: "http://localhost:4000/",
+    cache: new InMemoryCache(),
+    resolvers: {
+        Movie: {
+          isLiked: () => false
+        }
+    }
 });
 
 export default client;
